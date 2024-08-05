6a0a58aa7516f5b68e0737eafdeb4778e7eb4d4d
Deividas <kliucevicd@gmail.com>
2024-08-05 12:36:11 +0300

    commit

--- js/components/Layout.js
+++ js/components/Layout.js
@@ -1,85 +1,106 @@
 import { PageContact } from "./PageContact.js";
 import { PageHome } from "./PageHome.js";
 import { PageServices } from "./PageServices.js";
+import { PageShopAddNew } from "./PageShopAddNew.js";
+import { PageShopList } from "./PageShopList.js";
 import { PageTeam } from "./PageTeam.js";
 
 export class Layout {
     constructor() {
+        this.pagesData = [
+            {
+                text: 'Home',
+                content: PageHome,
+                background: 'pink',
+                title: 'Home',
+            },
+            {
+                text: 'Shop',
+                content: PageShopList,
+                background: 'white',
+                title: 'Your shopping list',
+            },
+            {
+                text: 'Add',
+                extraMenuStyle: 'menu-btn',
+                content: PageShopAddNew,
+                background: 'white',
+                title: 'Add new item to your shopping list',
+            },
+            // {
+            //     text: 'Team',
+            //     content: PageTeam,
+            //     background: 'grey',
+            //     title: 'Our team'
+            // },
+            // {
+            //     text: 'Services',
+            //     content: PageServices,
+            //     background: 'white',
+            //     title: 'Our services'
+            // },
+            // {
+            //     text: 'Contact us',
+            //     content: PageContact,
+            //     background: 'aquamarine',
+            //     title: 'Contact us',
+            // },
+        ];
+        this.DOM = document.getElementById('app');
+        this.mainDOM = null;
+
         this.render();
+        this.headerEvents();
+
+        new this.pagesData[1].content(this.mainDOM);
     }
 
     header() {
-        const HTML = `
+        let navHTML = '';
+
+        for (const link of this.pagesData) {
+            const style = link.extraMenuStyle ? link.extraMenuStyle : '';
+            navHTML += `<button class="link ${style}">${link.text}</button>`;
+        }
+
+        return `
             <header class="container main-header">
                 <div class="row">
                     <div class="col-12 main-header-content">
                         <img class="logo" src="./img/logo.png" alt="Logo">
-                        <nav class="hidden visible-sm-flex main-nav">
-                            <button class="link" data-page="home">Home</button>
-                            <button class="link" data-page="services">Services</button>
-                            <button class="link" data-page="team">Team</button>
-                            <button class="link" data-page="contact">Contact us</button>
+                        <nav class="main-nav">
+                            ${navHTML}
                         </nav>
                     </div>
                 </div>
             </header>`;
-        return HTML;
     }
 
     headerEvents() {
         const buttonsDOM = document.querySelectorAll('.main-header-content button');
+        this.mainDOM = document.querySelector('main.container');
 
-        for (const buttonDOM of buttonsDOM) {
-            buttonDOM.addEventListener('click', () => {
-                const page = buttonDOM.getAttribute('data-page');
-                this.loadPage(page);
+        for (let i = 0; i < buttonsDOM.length; i++) {
+            buttonsDOM[i].addEventListener('click', () => {
+                const pageClass = this.pagesData[i].content;
+                new pageClass(this.mainDOM);
             });
         }
     }
 
-    loadPage(page) {
-        let pageObject;
-        switch (page) {
-            case 'home':
-                pageObject = new PageHome();
-                break;
-            case 'services':
-                pageObject = new PageServices();
-                break;
-            case 'team':
-                pageObject = new PageTeam();
-                break;
-            case 'contact':
-                pageObject = new PageContact();
-                break;
-        }
-
-        const mainDOM = document.querySelector('main');
-        if (mainDOM) {
-            mainDOM.innerHTML = pageObject.render();
-        }
-    }
-
     main() {
-        const pageObject = new PageHome();
-        const HTML = `
+        return `
             <main class="container">
-                ${pageObject.render()}
+                EMPTY CONTENT
             </main>`;
-        return HTML;
     }
 
     footer() {
-        const HTML = '<footer class="container">&copy; Copyright 2024</footer>';
-        return HTML;
+        return '<footer class="container">&copy; Copyright 2024</footer>';
     }
 
     render() {
-        const DOM = document.getElementById('app');
         const HTML = this.header() + this.main() + this.footer();
-
-        DOM.insertAdjacentHTML('beforeend', HTML);
-
-        this.headerEvents();
+        this.DOM.insertAdjacentHTML('beforeend', HTML);
     }
 }
